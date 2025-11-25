4059dc831c86dc7bd30af156f01e1a817621d3a2
Kevin Dion <kevin@slashpine.xyz>
2025-11-25 09:09:10 -0500

    fix generate_version script for sh

--- generate_version.sh
+++ generate_version.sh
@@ -5,7 +5,7 @@
 fallback_version='0.0.0'
 version_file=$1
 
-if [[ -z "$version_file" ]]
+if [ -z "$version_file" ]
 then
     version_file="forgesteel_warehouse/__version__.py"
 fi
@@ -13,7 +13,7 @@ fi
 ## Get latest tag, trimming the 'v'
 latest_tag=$(git describe --abbrev=0 --tags --match 'v*.*.*' 2>/dev/null | sed 's/^v//')
 
-if [[ -z "$latest_tag" ]]
+if [ -z "$latest_tag" ]
 then
     latest_tag="$fallback_version"
 fi
@@ -21,7 +21,7 @@ fi
 ## see if current commit is tagged
 this_commit_tag=$(git tag -l 'v*.*.*' --points-at HEAD | sed 's/^v//')
 
-if [ "$this_commit_tag" == "$latest_tag" ]
+if [ "$this_commit_tag" = "$latest_tag" ]
 then
     version="$latest_tag"
 else
