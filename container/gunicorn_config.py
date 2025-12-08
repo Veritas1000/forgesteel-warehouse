2c4153283ffa8cdf3f2d33803e88bf0ec1d29e55
Kevin Dion <kevin@slashpine.xyz>
2025-12-08 06:20:15 -0500

    filter healthcheck from access logs in gunicorn

--- container/gunicorn_config.py
+++ container/gunicorn_config.py
@@ -1,4 +1,22 @@
+import logging
 import os
+import re
+
+class RequestPathFilter(logging.Filter):
+    def __init__(self, *args, path_re, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.path_filter = re.compile(path_re)
+
+    def filter(self, record):
+        req_path = record.args['U'] # type: ignore
+        if not self.path_filter.match(req_path):
+            return True  # log this entry
+        # ... additional conditions can be added here ...
+        return False     # do not log this entry
+
+
+def on_starting(server):
+    server.log.access_log.addFilter(RequestPathFilter(path_re=r'^/healthz$'))
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
