b09f8f9d84adfe9f16f7d85f9ff7586b9d7b0ed6
Kevin <kevin@slashpine.xyz>
2026-01-23 11:08:30 +0000

    Re-add proper Patreon scopes

--- forgesteel_warehouse/utils/patreon_api.py
+++ forgesteel_warehouse/utils/patreon_api.py
@@ -4,7 +4,7 @@ import os
 from dataclasses import dataclass, field
 from datetime import date, datetime
 from typing import List
-from urllib.parse import urlencode
+from urllib.parse import quote, urlencode
 
 import requests
 
@@ -36,27 +36,30 @@ class PatreonUser:
 
 class PatreonApi:
     _requested_token_scopes = [
-        'identity.memberships'
+        "identity",
+        "identity[email]",
+        "identity.memberships",
+        "campaigns",
     ]
 
     _identity_includes = [
-        'memberships.campaign',
-        'memberships.currently_entitled_tiers'
+        "memberships.campaign",
+        "memberships.currently_entitled_tiers",
     ]
     _member_fields = [
-        'patron_status',
-        'currently_entitled_amount_cents',
-        'pledge_relationship_start',
+        "patron_status",
+        "currently_entitled_amount_cents",
+        "pledge_relationship_start",
     ]
-    _user_fields = ['email']
-    _campaign_fields = ['url']
-    _tier_fields = ['title']
+    _user_fields = ["email"]
+    _campaign_fields = ["url"]
+    _tier_fields = ["title"]
 
     def __init__(self):
-        self.CLIENT_ID = os.getenv('PATREON_CLIENT_ID')
-        self.CLIENT_SECRET = os.getenv('PATREON_CLIENT_SECRET')
-        self.MCDM_CAMPAIGN_ID = os.getenv('PATREON_CAMPAIGN_ID_MCDM')
-        self.FORGESTEEL_CAMPAIGN_ID = os.getenv('PATREON_CAMPAIGN_ID_FORGESTEEL')
+        self.CLIENT_ID = os.getenv("PATREON_CLIENT_ID")
+        self.CLIENT_SECRET = os.getenv("PATREON_CLIENT_SECRET")
+        self.MCDM_CAMPAIGN_ID = os.getenv("PATREON_CAMPAIGN_ID_MCDM")
+        self.FORGESTEEL_CAMPAIGN_ID = os.getenv("PATREON_CAMPAIGN_ID_FORGESTEEL")
 
         if log.getEffectiveLevel() < logging.DEBUG:
             log.debug("setting up http debug logging")
@@ -65,13 +68,17 @@ class PatreonApi:
 
     def generate_authorize_url(self, redirect_uri, state):
         params = {
-            'response_type': 'code',
-            'client_id': self.CLIENT_ID,
-            'redirect_uri': redirect_uri,
-            'scope': ' '.join(self._requested_token_scopes),
-            'state': state,
+            "response_type": "code",
+            "client_id": self.CLIENT_ID,
+            "redirect_uri": redirect_uri,
+            "scope": " ".join(self._requested_token_scopes),
+            "state": state,
         }
-        url = f"https://www.patreon.com/oauth2/authorize?{urlencode(params)}"
+        url = f"https://www.patreon.com/oauth2/authorize?{urlencode(params, quote_via=quote)}"
+
+        if log.getEffectiveLevel() < logging.DEBUG:
+            log.debug(f"Authorization request URL is [{url}]")
+
         return url
 
     def get_token(self, auth_code, redirect_uri):
@@ -80,18 +87,16 @@ class PatreonApi:
             log.debug(f"Getting Patreon token with [{log_token}...]")
 
         response = requests.post(
-            'https://www.patreon.com/api/oauth2/token',
-            params = {
-                'grant_type': 'authorization_code',
-                'code': auth_code,
-                'client_id': self.CLIENT_ID,
-                'client_secret': self.CLIENT_SECRET,
-                'redirect_uri': redirect_uri
+            "https://www.patreon.com/api/oauth2/token",
+            params={
+                "grant_type": "authorization_code",
+                "code": auth_code,
+                "client_id": self.CLIENT_ID,
+                "client_secret": self.CLIENT_SECRET,
+                "redirect_uri": redirect_uri,
             },
-            headers = {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            timeout = 10
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            timeout=10,
         )
         if not response.ok:
             log.warning(f"Problem getting patreon token: {response.text}")
@@ -99,9 +104,14 @@ class PatreonApi:
         response.raise_for_status()
 
         tokens = response.json()
-        access_token = tokens['access_token']
-        refresh_token = tokens['refresh_token']
-        lifetime = tokens['expires_in']
+        access_token = tokens["access_token"]
+        refresh_token = tokens["refresh_token"]
+        lifetime = tokens["expires_in"]
+
+        if log.getEffectiveLevel() < logging.DEBUG:
+            log.debug(f"Got an access_token [{access_token[0:5]}...]")
+            log.debug(f"Token has scope(s) [{tokens["scope"]}]")
+
         return access_token, refresh_token, lifetime
 
     def refresh_token(self, refresh_token):
@@ -126,9 +136,9 @@ class PatreonApi:
         response.raise_for_status()
 
         tokens = response.json()
-        access_token = tokens['access_token']
-        refresh_token = tokens['refresh_token']
-        lifetime = tokens['expires_in']
+        access_token = tokens["access_token"]
+        refresh_token = tokens["refresh_token"]
+        lifetime = tokens["expires_in"]
         return access_token, refresh_token, lifetime
 
     def get_identity(self, access_token):
@@ -137,18 +147,16 @@ class PatreonApi:
             log.debug(f"Getting Patreon identity with [{log_token}...]")
 
         response = requests.get(
-            'https://www.patreon.com/api/oauth2/v2/identity',
-            params = {
-                'include': ','.join(self._identity_includes),
-                'fields[member]': ','.join(self._member_fields),
-                'fields[user]': ','.join(self._user_fields),
-                'fields[campaign]': ','.join(self._campaign_fields),
-                'fields[tier]': ','.join(self._tier_fields),
+            "https://www.patreon.com/api/oauth2/v2/identity",
+            params={
+                "include": ",".join(self._identity_includes),
+                "fields[member]": ",".join(self._member_fields),
+                "fields[user]": ",".join(self._user_fields),
+                "fields[campaign]": ",".join(self._campaign_fields),
+                "fields[tier]": ",".join(self._tier_fields),
             },
-            headers = {
-                'Authorization': f"Bearer {access_token}"
-            },
-            timeout = 10
+            headers={"Authorization": f"Bearer {access_token}"},
+            timeout=10,
         )
         if not response.ok:
             log.warning(f"Problem getting patreon identity: {response.text}")
@@ -162,53 +170,78 @@ class PatreonApi:
         patreon_id = None
         patreon_email = None
 
-        if identity_json is not None and 'data' in identity_json:
-            if 'id' in identity_json['data']:
-                patreon_id = identity_json['data']['id']
+        if identity_json is not None and "data" in identity_json:
+            if "id" in identity_json["data"]:
+                patreon_id = identity_json["data"]["id"]
 
-            if 'attributes' in identity_json['data'] and 'email' in identity_json['data']['attributes']:
-                patreon_email = identity_json['data']['attributes']['email']
+            if (
+                "attributes" in identity_json["data"]
+                and "email" in identity_json["data"]["attributes"]
+            ):
+                patreon_email = identity_json["data"]["attributes"]["email"]
 
         forgesteel_state = PatronState()
         mcdm_state = PatronState()
 
         ## find the campaigns we care about
-        if identity_json is not None and 'included' in identity_json:
-            memberships = list(filter(lambda inc: inc['type'] == 'member'
-                                        and 'relationships' in inc
-                                        and 'campaign' in inc['relationships']
-                                        and 'currently_entitled_tiers' in inc['relationships']
-                                        and 'data' in inc['relationships']['campaign']
-                                        and 'id' in inc['relationships']['campaign']['data'],
-                                        identity_json['included'])
-                                    )
+        if identity_json is not None and "included" in identity_json:
+            memberships = list(
+                filter(
+                    lambda inc: inc["type"] == "member"
+                    and "relationships" in inc
+                    and "campaign" in inc["relationships"]
+                    and "currently_entitled_tiers" in inc["relationships"]
+                    and "data" in inc["relationships"]["campaign"]
+                    and "id" in inc["relationships"]["campaign"]["data"],
+                    identity_json["included"],
+                )
+            )
 
-            tiers = list(filter(lambda inc: inc['type'] == 'tier',
-                                identity_json['included'])
-                            )
-            tier_map = dict(map(lambda tier: (tier['id'], tier['attributes']['title']), tiers))
+            tiers = list(
+                filter(lambda inc: inc["type"] == "tier", identity_json["included"])
+            )
+            tier_map = dict(
+                map(lambda tier: (tier["id"], tier["attributes"]["title"]), tiers)
+            )
 
-            forgesteel_state = self._get_patron_state(self.FORGESTEEL_CAMPAIGN_ID, memberships, tier_map)
-            mcdm_state = self._get_patron_state(self.MCDM_CAMPAIGN_ID, memberships, tier_map)
+            forgesteel_state = self._get_patron_state(
+                self.FORGESTEEL_CAMPAIGN_ID, memberships, tier_map
+            )
+            mcdm_state = self._get_patron_state(
+                self.MCDM_CAMPAIGN_ID, memberships, tier_map
+            )
 
         return PatreonUser(
             id=patreon_id,
             email=patreon_email,
             forgesteel=forgesteel_state,
-            mcdm=mcdm_state
+            mcdm=mcdm_state,
         )
 
     def _get_patron_state(self, campaign_id, memberships, all_tiers):
         patron_state = PatronState()
 
         for membership in memberships:
-            if membership['relationships']['campaign']['data']['id'] == campaign_id:
-                patron_state.patron = membership['attributes']['patron_status'] == 'active_patron'
+            if membership["relationships"]["campaign"]["data"]["id"] == campaign_id:
+                patron_state.patron = (
+                    membership["attributes"]["patron_status"] == "active_patron"
+                )
 
                 if patron_state.patron:
-                    tiers = list(t['id'] for t in membership['relationships']['currently_entitled_tiers']['data'])
-                    patron_state.tiers = list(map(lambda t: PatronTier(id=t, title=all_tiers.get(t)), tiers))
-                    patron_state.tier_cents = membership['attributes']['currently_entitled_amount_cents']
-                    patron_state.start = datetime.fromisoformat(membership['attributes']['pledge_relationship_start']).date()
+                    tiers = list(
+                        t["id"]
+                        for t in membership["relationships"][
+                            "currently_entitled_tiers"
+                        ]["data"]
+                    )
+                    patron_state.tiers = list(
+                        map(lambda t: PatronTier(id=t, title=all_tiers.get(t)), tiers)
+                    )
+                    patron_state.tier_cents = membership["attributes"][
+                        "currently_entitled_amount_cents"
+                    ]
+                    patron_state.start = datetime.fromisoformat(
+                        membership["attributes"]["pledge_relationship_start"]
+                    ).date()
 
         return patron_state
