1e147a861c35f707bc7a15493c2c56db0e421ac6
Kevin Dion <kdion7@gmail.com>
2026-04-18 13:28:47 -0400

    Improve dev_server script

--- dev_server.sh
+++ dev_server.sh
@@ -3,6 +3,26 @@
 PROJ_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PYTHONPATH="${PYTHONPATH}:${PROJ_DIR}"
 
+echo "===  Starting warehouse dev server  ==="
+echo "Using directory: $PROJ_DIR"
+
+## Do a simple check to see if there is a config dir set via env var or .env file
+echo "Checking for config path to be set"
+if [ -z "$FSW_CONFIG_PATH" ]
+then
+    echo "Not set as an env var. Checking for .env"
+    if ! `grep 'FSW_CONFIG_PATH' "${PROJ_DIR}/.env" > /dev/null 2>&1`
+    then
+        echo "Not found in .env"
+        echo "Creating a basic .env with a default config"
+        echo "FSW_CONFIG_PATH=./instance/config.json" > "${PROJ_DIR}/.env"
+    else
+        echo ".env file detected"
+    fi
+else
+    echo "FSW_CONFIG_PATH=${FSW_CONFIG_PATH}"
+fi
+
 python container/utils/bootstrap.py
 
 gunicorn --reload --config container/gunicorn_config.py 'forgesteel_warehouse:init_app()'
